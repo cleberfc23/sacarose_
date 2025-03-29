ab6a905cde22c8df5a0d9447dd016f032f41bf98
Cleber F. Carvalho <cleberfc23@gmail.com>
2025-03-29 11:21:10 +0900

    adding graph vis, metrics, chaging structure of the project

---
+++ main/graphic_visualization/plot_graphics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_graph_here():
+    # print(data.head())
+    print("plot graph")
+
+
+def clebinho(data):
+    print(data.head())
+    print("clebinho")
+
+
+def print_cabeca(data):
+    print(data.head())
